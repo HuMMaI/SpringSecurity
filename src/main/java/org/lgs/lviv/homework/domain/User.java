e15fab21fa50ef0fb1c4ccc5c7852b9e71d4d076
hummai <shining.melon2015@gmail.com>
2020-04-24 18:53:28 +0300

    Added user entity and user roles

---
+++ src/main/java/org/lgs/lviv/homework/domain/User.java
@@ -0,0 +1,96 @@
+package org.lgs.lviv.homework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Enumerated(EnumType.ORDINAL)
+    @ElementCollection(targetClass = UserRole.class, fetch = FetchType.EAGER)
+    @Column(name = "role")
+    private Set<UserRole> roles;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/org/lgs/lviv/homework/domain/UserRole.java
@@ -0,0 +1,6 @@
+package org.lgs.lviv.homework.domain;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
